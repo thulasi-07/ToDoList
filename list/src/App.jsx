a79911854150934201f21c38d91a9e2a4b15c823
Thulasi <thulasipanndeshwara02@gmail.com>
2024-11-13 12:08:01 +0530

    did

--- list/src/App.jsx
+++ list/src/App.jsx
@@ -2,33 +2,66 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const handleEdit = () => {};
+  const handleDelete = () => {};
+  const handleAdd = () => {
+    setTodos([...todos, { todo, isCompleted: false }]);
+    setTodo("");
+    console.log(todos);
+  };
+
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  };
   return (
     <>
       <Navbar />
       <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[70vh]">
-        <div className="addTodo">
+        <div className="addTodo" my-5>
           <h2 className="text-lg font-bold">Add a Todo</h2>
-          <input type="text" />
-          <button className="bg-violet-800 hover:bg-indigo-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-6">
+          <input
+            onChange={handleChange}
+            value={todo}
+            type="text"
+            className="w-80"
+          />
+          <button
+            onClick={handleAdd}
+            className="bg-violet-800 hover:bg-indigo-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-6"
+          >
             Add
           </button>
         </div>
         <h2 className="text-lg font-bold">Your ToDos</h2>
-     
-      <div className="todos">
-        <div className="todo flex">
-          <div className="text">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit enim
-            laboriosam, explicabo eveniet sed nemo nobis? Possimus molestias
-            magnam fugit reiciendis temporibus adipisci, rerum laborum
-            voluptatibus odit animi fugiat numquam!
-          </div>
-          <div className="buttons">
-            <button className="bg-violet-800 hover:bg-indigo-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1">Edit</button>
-            <button className="bg-violet-800 hover:bg-indigo-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1">Delete</button>
-          </div>
+
+        <div className="todos">
+          {todos.map((item) => {
+            return (
+              <div key={todo} className="todo flex w-full">
+                <div className={item.isCompleted ? "" : "line-through"}>
+                  {item.todo}
+                  my todo
+                </div>
+                <div className="buttons">
+                  <button
+                    onClick={handleEdit}
+                    className="bg-violet-800 hover:bg-indigo-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    onClick={handleEdit}
+                    className="bg-violet-800 hover:bg-indigo-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            );
+          })}
         </div>
-      </div>
       </div>
     </>
   );
--- list/src/components/Navbar.jsx
+++ list/src/components/Navbar.jsx
@@ -3,13 +3,15 @@ import React from "react";
 export default function Navbar() {
   return (
     <nav className="flex justify-between bg-slate-700 text-white">
-        <div className="logo">
-            <span className="font-bold text-xl mx-8">iTask</span>
-        </div>
+      <div className="logo">
+        <span className="font-bold text-xl mx-8">iTask</span>
+      </div>
 
       <ul className="flex gap-8 mx-9">
         <li className="cursor-pointer hover:font-bold transition-all">Home</li>
-        <li className="cursor-pointer hover:font-bold transition-all">Your Tasks</li>
+        <li className="cursor-pointer hover:font-bold transition-all">
+          Your Tasks
+        </li>
       </ul>
     </nav>
   );
