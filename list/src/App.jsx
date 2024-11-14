92deec224c38a52435022a4683344acce34f6c06
Thulasi <thulasipanndeshwara02@gmail.com>
2024-11-14 16:07:30 +0530

    done

--- list/src/App.jsx
+++ list/src/App.jsx
@@ -5,8 +5,21 @@ import { v4 as uuidv4 } from "uuid";
 function App() {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
-  const handleEdit = () => {};
-  const handleDelete = () => {};
+
+
+  const handleEdit = () => {
+   
+
+  
+  const handleDelete = (e, id) => {
+    let newTodos = todos.filter(item=> {
+      return item.id!==id
+    });
+    setTodos(newTodos);
+  };
+    
+    
+  };
   const handleAdd = () => {
     setTodos([...todos, { id: uuidv4(), todo, isCompleted: false }]);
     setTodo("");
@@ -17,14 +30,17 @@ function App() {
     setTodo(e.target.value);
   };
   const handleCheckbox = (e) => {
+    console.log(e, e.target)
     let id = e.target.name;
-    console.log(id);
+    console.log('The id is ${id}');
     let index = todos.findIndex((item) => {
       return item.id === id;
     });
-    let newTodos = todos;
+    console.log(index)
+    let newTodos = [...todos];
     newTodos[index].isCompleted = !newTodos[index].isCompleted;
     setTodos(newTodos);
+    console.log(newTodos)
   };
 
   return (
@@ -43,25 +59,27 @@ function App() {
             onClick={handleAdd}
             className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-6"
           >
-            Add
+            Save 
           </button>
         </div>
         <h2 className="text-lg font-bold">Your ToDos</h2>
 
         <div className="todos">
+        {todos.length===0 && <div className="m-5">No ToDos To Display</div>}
+
           {todos.map((item) => {
-            return;
-            <div key={todo.id} className="todo flex w-1/4 my-3 justify-between">
+            return <div key={item.id} className="todo flex w-1/4 my-3 justify-between">
+              <div className="flex gap-5">
               <input
-                name={todo.id}
+                name={item.id}
                 onChange={handleCheckbox}
                 type="checkbox"
-                value={todo.isCompleted}
+                value={item.isCompleted}
                 id=""
               />
               <div className={item.isCompleted ? "line-through" : ""}>
                 {item.todo}
-                my todo
+              </div>
               </div>
               <div className="buttons">
                 <button
@@ -71,13 +89,14 @@ function App() {
                   Edit
                 </button>
                 <button
-                  onClick={handleEdit}
+                  onClick={(e)=>{handleDelete(e, item.id)}}
                   className="bg-violet-800 hover:bg-indigo-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1"
                 >
                   Delete
                 </button>
               </div>
-            </div>;
+            </div>
+
           })}
         </div>
       </div>
