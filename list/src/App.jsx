e466c6a2750439d60e43543931d5b70dd9827611
Thulasi <thulasipanndeshwara02@gmail.com>
2024-11-18 21:17:33 +0530

    lit

--- list/src/App.jsx
+++ list/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState , useEffect} from "react";
 import Navbar from "./components/Navbar";
+import { FaEdit } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
 import { v4 as uuidv4 } from "uuid";
 
 function App() {
@@ -16,15 +18,12 @@ useEffect(() => {
   }
 }, [])
 
-
-
   const saveTols = (params) => {
     localStorage.setItem("todos", JSON.stringify(todos))
   }
 
   const toggleFinished = (e) => {
     setshowFinished(!showFinished)
-
   }
 
 
@@ -42,53 +41,57 @@ useEffect(() => {
     let newTodos = todos.filter(item=> {
       return item.id!==id
     });
-    setTodos(newTodos);
+    setTodos(newTodos)
     saveTols()
-  };
-    
-    
-  };
-  const handleAdd = () => {
-    setTodos([...todos, { id: uuidv4(), todo, isCompleted: false }]);
+  }
+  const handleAdd = ()=> {
+    setTodos([...todos, { id: uuidv4(), todo, isCompleted: false }])
     setTodo("");
     saveTols()
-  };
+  }
 
   const handleChange = (e) => {
     setTodo(e.target.value);
-  };
+  }
+
   const handleCheckbox = (e) => {
-    console.log(e, e.target)
     let id = e.target.name;
-    let index = todos.findIndex((item) => {
+    let index = todos.findIndex(item=> {
       return item.id === id;
-    });
-    console.log(index)
+    })
     let newTodos = [...todos];
     newTodos[index].isCompleted = !newTodos[index].isCompleted;
     setTodos(newTodos);
     saveTols()
-  };
+  }
 
   return (
     <>
       <Navbar />
-      <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh] w-1/2">
-        <div className="addTodo" my-5 flex flex-col gap-4>
-          <h2 className="text-lg font-bold">Add a Todo</h2>
+      <div className="mx-3 md:container md:mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh] md:w-[35%]">
+        <h1 className="font-bold text-center text-3xl">iTask - Manage your todos at one place</h1>
+        <div className="addTodo my-5 flex flex-col gap-4">
+          <h2 className="text-2xl font-bold">Add a Todo</h2>
+          <div className="flex">
           <input
             onChange={handleChange}
             value={todo}
             type="text"
-            className="w-full"
+            className="w-full rounded-full px-5 py-1"
           />
           <button
             onClick={handleAdd} disabled={todo.length<=3}
-            className="bg-violet-800 disabled:bg-violet-700 hover:bg-violet-900 disabled:via-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-6"
+            className="bg-violet-800 mx-2 rounded-full hover:bg-violet-950 disabled:via-violet-500 disabled:bg-violet-500 p-2 py-2 text-sm font-bold text-white"
           >
             Save 
           </button>
         </div>
+        </div>
+
+
+
+
+        
         <input onChange={toggleFinished} type="checkbox" checked={showFinished}/>Show Finished
         <h2 className="text-lg font-bold">Your ToDos</h2>
 
